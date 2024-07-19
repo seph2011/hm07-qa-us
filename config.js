c1ea4d09c3b2216fd1a5b5f3cbabc63aee9f3783
redbeard <seph2011@gmail.com>
2024-07-18 20:43:01 -0500

    correcting URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-10c1d1ee-4e44-42ee-9da3-ddfa0996a6a5.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-c24f5396-67ca-4f36-9a1d-38de98e304db.containerhub.tripleten-services.com"
 }
